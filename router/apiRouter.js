f934892c1971e0d48e259ce2dd134cf1a08195af
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-03-30 23:49:18 +0200

    fix controller route

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {eventsController}  = require('./controller/apiController');
+const {eventsController}  = require('../controller/apiController');
 
 router.get('/events' ,eventsController);
 
